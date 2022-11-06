3d9368f728b82145caa44ce704bd437a5151c873
AnuruddhaBanadara <anuruddha@prodoscore.com>
2022-11-06 23:41:45 +0530

    chore: update basic changes

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,5 +4,6 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 def home_page(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, "home.html")
+
 
