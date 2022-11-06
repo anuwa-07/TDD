694c5325e0a5cc450bf26066672af1c3f809caff
AnuruddhaBanadara <anuruddha@prodoscore.com>
2022-11-06 21:50:54 +0530

    chore: update basic changes

---
+++ superlists/lists/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self) -> None:
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
